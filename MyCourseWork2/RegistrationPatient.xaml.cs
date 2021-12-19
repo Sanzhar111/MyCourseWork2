f0a43947f4c94a626e78ced1cd2100ccec34e993
Ярослав Смирнов <d3156@mail.ru>
2021-12-19 22:45:27 +0300

    Бла Бла бла

--- MyCourseWork2/RegistrationPatient.xaml.cs
+++ MyCourseWork2/RegistrationPatient.xaml.cs
@@ -1,18 +1,6 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MyCourseWork2
 {
@@ -39,13 +27,13 @@ namespace MyCourseWork2
             MySqlCommand command = new MySqlCommand("SELECT Address, Date_Of_Illnes, " +
                 "(SELECT Name from illness where idillness=patients.Diagnosis)" +
                 "as Diagnos FROM patients where idPatient=@uL;", db.getConnection());
+            /*command.Parameters.Add("@", MySqlDbType.VarChar).Value = ;
             command.Parameters.Add("@", MySqlDbType.VarChar).Value = ;
             command.Parameters.Add("@", MySqlDbType.VarChar).Value = ;
             command.Parameters.Add("@", MySqlDbType.VarChar).Value = ;
             command.Parameters.Add("@", MySqlDbType.VarChar).Value = ;
             command.Parameters.Add("@", MySqlDbType.VarChar).Value = ;
-            command.Parameters.Add("@", MySqlDbType.VarChar).Value = ;
-            command.Parameters.Add("@", MySqlDbType.VarChar).Value = ;
+            command.Parameters.Add("@", MySqlDbType.VarChar).Value = ;*/
             MySqlDataAdapter adapter = new MySqlDataAdapter(command);
             DataTable data = new DataTable("patients");
             adapter.Fill(data);
